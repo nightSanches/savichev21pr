58d120847a9180b38ba3acb9d9e0cfb8b09a9cb3
gsavichev <galesavich@gmail.com>
2024-01-23 21:04:34 +0500

    Реализованные модели. Савичев

---
+++ Models/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev21pr.Models
+{
+    public class Document
+    {
+        public int id {  get; set; }
+        public string src { get; set; }
+        public string name { get; set; }
+        public string user { get; set; }
+        public int id_document { get; set; }
+        public DateTime date { get; set; }
+        public int status { get; set; }
+        public string vector { get; set; }
+    }
+}
