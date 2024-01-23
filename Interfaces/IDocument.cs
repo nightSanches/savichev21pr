f5c5870b0ca55c29b470db55e90cec2a9414e522
gsavichev <galesavich@gmail.com>
2024-01-23 21:07:26 +0500

    Реализованные интерфейсы. Савичев

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev21pr.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Documents.Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
