fb8d8cc910203dbfdcd02474ea085015930d775e
gsavichev <galesavich@gmail.com>
2024-01-23 22:55:25 +0500

    Реализованная страница Main. Савичев

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,46 @@
+﻿using savichev21pr.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace savichev21pr.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreatedUI();
+        }
+        public void CreatedUI()
+        {
+            parent.Children.Clear();
+            foreach (DocumentContext document in MainWindow.init.AllDocuments)
+                parent.Children.Add(new Elements.Item(document));
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+
+        private void AddDocuments(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.add);
+        }
+    }
+}
