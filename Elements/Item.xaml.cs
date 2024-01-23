aa408601c7993f3e42076e9218d64e2a2b9cc092
gsavichev <galesavich@gmail.com>
2024-01-23 22:41:19 +0500

    Реализованные пользовательские элементы управления. Савичев

---
+++ Elements/Item.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using savichev21pr.Classes;
+
+namespace savichev21pr.Elements
+{
+    public partial class Item : UserControl
+    {
+        DocumentContext Document;
+        public Item(DocumentContext Document)
+        {
+            InitializeComponent();
+            img.Source = new BitmapImage(new Uri(Document.src));
+            lName.Content = Document.name;
+            lUser.Content = $"Ответственный: {Document.user}";
+            lCode.Content = $"Код документа: {Document.id_document}";
+            lDate.Content = $"Дата поступления {Document.date.ToString("dd.MM.yyyy")}";
+            lStatus.Content = Document.status == 0 ? "Статус: входящий" : "Статус: исходящий";
+            lDirect.Content = "Направление: " + Document.vector;
+            this.Document = Document;
+        }
+
+        private void EditDocument(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.frame.Navigate(new Pages.Add(Document));
+        }
+
+        private void DeleteDocument(object sender, RoutedEventArgs e)
+        {
+            Document.Delete();
+            MainWindow.init.AllDocuments = new DocumentContext().AllDocuments();
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+        }
+    }
+}
